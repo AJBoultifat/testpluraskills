3ae7ec106edcd19652c1ce0bf2b80a453a4a44e0
AJBoultifat <64844745+AJBoultifat@users.noreply.github.com>
2021-11-12 12:40:32 +0100

    Update abdeldjalil-boultifat-test.py

--- abdeldjalil-boultifat-test.py
+++ abdeldjalil-boultifat-test.py
@@ -1,13 +1,18 @@
 import random
 
+'''List void'''
 List = []
 
+'''Filing the list with int random value for 1000 times'''
 for i in range(0, int(1000)):
     n = random.randint(0,1000)
     List.append(n)
 
+    
+'''Print the list to confirm that it is well appended'''
 print(List)
 
+'''Calculation'''
 i = 0
 for l in List:
     if((l%3 == 0) and (l%5 == 0)): print(str(i) +': '+ str(l) + ' is FizzBuzz')
