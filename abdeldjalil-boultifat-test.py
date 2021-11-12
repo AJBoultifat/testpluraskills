a84834fdaf49a79b300009cd8cdfea463ea96926
AJBoultifat <64844745+AJBoultifat@users.noreply.github.com>
2021-11-12 12:46:40 +0100

    Update abdeldjalil-boultifat-test.py

--- abdeldjalil-boultifat-test.py
+++ abdeldjalil-boultifat-test.py
@@ -1,4 +1,16 @@
+
 import random
+
+'''Calculation'''
+def functionTest(List):
+    i = 0
+    for l in List:
+        if((l%3 == 0) and (l%5 == 0)): print(str(i) +': '+ str(l) + ' is FizzBuzz')
+        if(l%5 == 0): print(str(i) +': '+str(l) +' is Buzz')
+        if(l%3 == 0): print(str(i) +': '+str(l) +' is Fizz')
+        if((l%3 != 0) and (l%5 != 0)): print(str(i) +': '+str(l))
+        i = i + 1
+    
 
 '''List void'''
 List = []
@@ -15,14 +27,3 @@ print(List)
 print('Calculate :')
 functionTest(List)
 
-'''Calculation'''
-def functionTest(List):
-    i = 0
-    for l in List:
-        if((l%3 == 0) and (l%5 == 0)): print(str(i) +': '+ str(l) + ' is FizzBuzz')
-        if(l%5 == 0): print(str(i) +': '+str(l) +' is Buzz')
-        if(l%3 == 0): print(str(i) +': '+str(l) +' is Fizz')
-        if((l%3 != 0) and (l%5 != 0)): print(str(i) +': '+str(l))
-        i = i + 1
-    
-    
