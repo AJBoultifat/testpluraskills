606c318b6d56cfb1422cd0513010c8c31fb7bce2
AJBoultifat <64844745+AJBoultifat@users.noreply.github.com>
2021-11-12 12:58:47 +0100

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,11 @@
+'''Calculation'''
+class calculate(object):
+    @staticmethod
+    def functionTest(List):
+        i = 0
+        for l in List:
+            if((l%3 == 0) and (l%5 == 0)): print(str(i) +': '+ str(l) + ' is FizzBuzz')
+            if(l%5 == 0): print(str(i) +': '+str(l) +' is Buzz')
+            if(l%3 == 0): print(str(i) +': '+str(l) +' is Fizz')
+            if((l%3 != 0) and (l%5 != 0)): print(str(i) +': '+str(l))
+            i = i + 1
