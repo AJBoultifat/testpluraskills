3dab0be05006b1dee58be465622296b61594d742
AJBoultifat <64844745+AJBoultifat@users.noreply.github.com>
2021-11-12 12:45:38 +0100

    Update abdeldjalil-boultifat-test.py

--- abdeldjalil-boultifat-test.py
+++ abdeldjalil-boultifat-test.py
@@ -12,14 +12,17 @@ for i in range(0, int(1000)):
 '''Print the list to confirm that it is well appended'''
 print('The list :')
 print(List)
+print('Calculate :')
+functionTest(List)
 
 '''Calculation'''
-i = 0
-for l in List:
-    if((l%3 == 0) and (l%5 == 0)): print(str(i) +': '+ str(l) + ' is FizzBuzz')
-    if(l%5 == 0): print(str(i) +': '+str(l) +' is Buzz')
-    if(l%3 == 0): print(str(i) +': '+str(l) +' is Fizz')
-    if((l%3 != 0) and (l%5 != 0)): print(str(i) +': '+str(l))
-    i = i + 1
+def functionTest(List):
+    i = 0
+    for l in List:
+        if((l%3 == 0) and (l%5 == 0)): print(str(i) +': '+ str(l) + ' is FizzBuzz')
+        if(l%5 == 0): print(str(i) +': '+str(l) +' is Buzz')
+        if(l%3 == 0): print(str(i) +': '+str(l) +' is Fizz')
+        if((l%3 != 0) and (l%5 != 0)): print(str(i) +': '+str(l))
+        i = i + 1
     
     
